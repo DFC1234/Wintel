06229138330cbf7ddb4675595b8e56cc2813bcdc
name <email>
2024-08-31 17:34:38 +0800

    version1.0

---
+++ cmake-build-debug/CMakeFiles/Wintel.elf.dir/DependInfo.cmake
@@ -0,0 +1,78 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/Users/ding/STM32/Wintel/Core/Startup/startup_stm32g030f6px.s" "/Users/ding/STM32/Wintel/cmake-build-debug/CMakeFiles/Wintel.elf.dir/Core/Startup/startup_stm32g030f6px.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32G030xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/Users/ding/STM32/Wintel/Core/Inc"
+  "/Users/ding/STM32/Wintel/Drivers/STM32G0xx_HAL_Driver/Inc"
+  "/Users/ding/STM32/Wintel/Drivers/STM32G0xx_HAL_Driver/Inc/Legacy"
+  "/Users/ding/STM32/Wintel/Drivers/CMSIS/Device/ST/STM32G0xx/Include"
+  "/Users/ding/STM32/Wintel/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/ding/STM32/Wintel/Core/Src/adc.c" "CMakeFiles/Wintel.elf.dir/Core/Src/adc.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Core/Src/adc.c.obj.d"
+  "/Users/ding/STM32/Wintel/Core/Src/dma.c" "CMakeFiles/Wintel.elf.dir/Core/Src/dma.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Core/Src/dma.c.obj.d"
+  "/Users/ding/STM32/Wintel/Core/Src/gpio.c" "CMakeFiles/Wintel.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Core/Src/gpio.c.obj.d"
+  "/Users/ding/STM32/Wintel/Core/Src/i2c.c" "CMakeFiles/Wintel.elf.dir/Core/Src/i2c.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Core/Src/i2c.c.obj.d"
+  "/Users/ding/STM32/Wintel/Core/Src/main.c" "CMakeFiles/Wintel.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Core/Src/main.c.obj.d"
+  "/Users/ding/STM32/Wintel/Core/Src/stm32g0xx_hal_msp.c" "CMakeFiles/Wintel.elf.dir/Core/Src/stm32g0xx_hal_msp.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Core/Src/stm32g0xx_hal_msp.c.obj.d"
+  "/Users/ding/STM32/Wintel/Core/Src/stm32g0xx_it.c" "CMakeFiles/Wintel.elf.dir/Core/Src/stm32g0xx_it.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Core/Src/stm32g0xx_it.c.obj.d"
+  "/Users/ding/STM32/Wintel/Core/Src/syscalls.c" "CMakeFiles/Wintel.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Core/Src/syscalls.c.obj.d"
+  "/Users/ding/STM32/Wintel/Core/Src/sysmem.c" "CMakeFiles/Wintel.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Core/Src/sysmem.c.obj.d"
+  "/Users/ding/STM32/Wintel/Core/Src/system_stm32g0xx.c" "CMakeFiles/Wintel.elf.dir/Core/Src/system_stm32g0xx.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Core/Src/system_stm32g0xx.c.obj.d"
+  "/Users/ding/STM32/Wintel/Core/Src/tim.c" "CMakeFiles/Wintel.elf.dir/Core/Src/tim.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Core/Src/tim.c.obj.d"
+  "/Users/ding/STM32/Wintel/Core/Src/usart.c" "CMakeFiles/Wintel.elf.dir/Core/Src/usart.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Core/Src/usart.c.obj.d"
+  "/Users/ding/STM32/Wintel/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal.c" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal.c.obj.d"
+  "/Users/ding/STM32/Wintel/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_adc.c" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_adc.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_adc.c.obj.d"
+  "/Users/ding/STM32/Wintel/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_adc_ex.c" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_adc_ex.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_adc_ex.c.obj.d"
+  "/Users/ding/STM32/Wintel/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_cortex.c" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_cortex.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_cortex.c.obj.d"
+  "/Users/ding/STM32/Wintel/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma.c" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma.c.obj.d"
+  "/Users/ding/STM32/Wintel/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma_ex.c" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_dma_ex.c.obj.d"
+  "/Users/ding/STM32/Wintel/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_exti.c" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_exti.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_exti.c.obj.d"
+  "/Users/ding/STM32/Wintel/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash.c" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash.c.obj.d"
+  "/Users/ding/STM32/Wintel/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash_ex.c" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_flash_ex.c.obj.d"
+  "/Users/ding/STM32/Wintel/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_gpio.c" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_gpio.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_gpio.c.obj.d"
+  "/Users/ding/STM32/Wintel/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_i2c.c" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_i2c.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_i2c.c.obj.d"
+  "/Users/ding/STM32/Wintel/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_i2c_ex.c" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_i2c_ex.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_i2c_ex.c.obj.d"
+  "/Users/ding/STM32/Wintel/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr.c" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr.c.obj.d"
+  "/Users/ding/STM32/Wintel/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr_ex.c" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_pwr_ex.c.obj.d"
+  "/Users/ding/STM32/Wintel/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc.c" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc.c.obj.d"
+  "/Users/ding/STM32/Wintel/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc_ex.c" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_rcc_ex.c.obj.d"
+  "/Users/ding/STM32/Wintel/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim.c" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim.c.obj.d"
+  "/Users/ding/STM32/Wintel/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim_ex.c" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_tim_ex.c.obj.d"
+  "/Users/ding/STM32/Wintel/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_uart.c" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_uart.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_uart.c.obj.d"
+  "/Users/ding/STM32/Wintel/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_uart_ex.c" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_uart_ex.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_hal_uart_ex.c.obj.d"
+  "/Users/ding/STM32/Wintel/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_adc.c" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_adc.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_adc.c.obj.d"
+  "/Users/ding/STM32/Wintel/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_rcc.c" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_rcc.c.obj" "gcc" "CMakeFiles/Wintel.elf.dir/Drivers/STM32G0xx_HAL_Driver/Src/stm32g0xx_ll_rcc.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
